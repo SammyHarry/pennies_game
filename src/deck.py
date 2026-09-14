fc2f032fa8b56429d2023da42c553dd2e430967e
SammyHarry <seharrington@wm.edu>
2026-09-14 12:11:43 -0400

    seed logging
    added what we did from class

--- src/deck.py
+++ src/deck.py
@@ -1,11 +1,13 @@
+import json
 import time
-
+from matplotlib.path import Path
 import numpy as np
-import struct
 
 class Deck:
     def __init__(self, num_decks: int = 1):
         self.cards = self._generate_deck(num_decks)
+        self.PATH_SEED_LOG = Path('data/seed_log.json')
+        self.PATH_DECK_LOG = Path('data/deck_log.json')
 
     def _generate_deck(self, num_decks: int):
         shape = (52, num_decks)
@@ -17,11 +19,32 @@ class Deck:
     def shuffle(self):
         rng = np.random.default_rng()
 
-        self.decks = rng.permuted(self.decks, axis=0)
+        self.decks = rng.permuted(self.decks, axis=0, seed=self.get_next_seed())
         return self.decks
 
     def save_decks(self, filename: str):
         np.savez_compressed(filename, decks=self.decks)
+
+    def get_next_seed(self) -> int:
+        #make sure parent dir exists
+        self.PATH_SEED_LOG.parent.mkdir(parents=True, exist_ok=True)
+
+        #determine next seed
+        if not self.PATH_SEED_LOG.exists():
+            print(f'NO seed found stat w {0}')
+            self.seed = 0
+        else:
+            with self.PATH_SEED_LOG.open('r') as f:
+                seed_log = json.load(f)
+            self.seed = seed_log['seed'] +1
+
+        seed_log = {
+            'seed' : self.seed,
+            'seed_time': str(time.ctime())
+        }
+        with self.PATH_SEED_LOG.open('w') as f:
+                json.dump(seed_log, f)
+        return self.seed
 
 
 
@@ -29,7 +52,7 @@ class Deck:
 def main(num_decks: int = 100):
     deck = Deck(num_decks=num_decks)
     deck.shuffle()
-    deck.save_decks(f"data/decks_{num_decks}_{time.ctime()}.npz")
+    deck.save_decks(f"data/decks_{num_decks}_{time.ctime()}_seed_{deck.seed}.npz")
 
 
 if __name__ == '__main__':
--- src/scoring.py
+++ src/scoring.py
@@ -3,13 +3,15 @@ import numpy as np
 
 class scoring:
     def init(self):
-       self.score = np.zeros(shape = (8, 8, 4))
+       self.score = np.zeros(shape = (8, 8, 4)) #stragegies x strategies x [p1 wins, ties, avg tricks of p1, avg length of trick of p1]
        self.strategies = [
         ['R', 'R', 'R'], ['B', 'B', 'B'],
         ['R', 'B', 'B'], ['B', 'B', 'R'],
         ['R', 'R', 'B'], ['B', 'R', 'R'],
         ['R', 'B', 'R'], ['B', 'R', 'B'],
         ]
+       
+       
        
        
 
